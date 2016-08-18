c3fa3c953db685c745c276d4715211219af9a87c
Your Name <you@example.com>
2016-08-18 16:37:40 +0900

    update

--- scrapy/sample.py
+++ scrapy/sample.py
@@ -4,24 +4,20 @@ import scrapy
 
 
 class QiitaSpider(scrapy.Spider):
-    name = 'qiita_spider'
+    name = 'casestudy_spider'
 
-    # Gh|CgiN[OðJn·éURLðLÚ·éj
-    start_urls = ['http://qiita.com/advent-calendar/2015/categories/programming_languages']
+    start_urls = ['http://preview.tis.web-meister.jp/wmpreview/www.tis.jp/casestudy/']
 
     custom_settings = {
         "DOWNLOAD_DELAY": 1,
     }
 
-    # URLÌoðLÚ
     def parse(self, response):
         for href in response.css('.adventCalendarList .adventCalendarList_calendarTitle > a::attr(href)'):
             full_url = response.urljoin(href.extract())
 
-            # oµ½URLð³ÉRequestðì¬µA_E[h·é
             yield scrapy.Request(full_url, callback=self.parse_item)
 
-    # _E[hµ½y[Wð³ÉAàeðoµÛ¶·éItemðì¬
     def parse_item(self, response):
 
         urls = []
