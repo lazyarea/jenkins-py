95c6916fd990fefdfcb22ea50866d148541b1354
Your Name <you@example.com>
2016-08-18 17:40:06 +0900

    update

--- selenium/test_casestudy.py
+++ selenium/test_casestudy.py
@@ -1,15 +1,34 @@
 # -*- conding:utf8 -*-
-
+import os
 import unittest
-import Crawler
+import urllib3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
-class SimpleTest(unittest.TestCase):
+class TestSimple(unittest.TestCase):
+
+    def setUp(self):
+        self.wait = 10
+        self.url = 'http://preview.tis.web-meister.jp/wmpreview/www.tis.jp/casestudy/'
+        self.browser = webdriver.Chrome(os.getcwd() + '/lib/chromedriver')
+        pass
+
+    def tearDown(self):
+        self.browser.quit()
+        pass
 
     def test1(self):
+        self.browser.get( self.url )
+        try:
+            elem = WebDriverWait(self.browser, self.wait).until(
+                    EC.presence_of_element_located((By.ID, "selKey1"))
+                    )
+        finally:
+            self.browser.quit()
 
-        url = 'http://someurl'
-        crawl = Crawler.Crawler()
-        crawl.main(url)
 
         self.assertEqual(1,1)
 
