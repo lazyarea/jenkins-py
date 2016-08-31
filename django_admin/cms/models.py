2569917297f244c7e3507414777edfd38e09a231
Your Name <you@example.com>
2016-09-01 06:05:11 +0900

    aa

--- django_admin/cms/models.py
+++ django_admin/cms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+import json
+
+def load_json_from_file(path):
+    with open(path, 'r') as f:
+        obj = json.load(f)
+        print (json.dumps(obj))
+
+
+def take_shot(driver, name='', dir='screenshots/', ext='.png'):
+    path = dir + name + ext
+    driver.save_screenshot(path)
+
+
+def wait():
+     wait = WebDriverWait(driver, 3)
+     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".pagination")))
