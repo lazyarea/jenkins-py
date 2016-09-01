2ca500c700966aca67d35776026b8b59599004f2
Your Name <you@example.com>
2016-09-02 01:18:54 +0900

    add screenshot

--- sample/utils.py
+++ sample/utils.py
@@ -6,3 +6,8 @@ def load_json_from_file(path):
     with open(path, 'r') as f:
         obj = json.load(f)
         print (json.dumps(obj))
+
+def take_shot(driver, name='', dir='screenshots/', ext='.png'):
+    path = dir + name + ext
+    driver.save_screenshot(path)
+
